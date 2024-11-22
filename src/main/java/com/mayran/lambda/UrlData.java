7a5b31dc5afa3e0c8def31f24ce73e11390ac0ba
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-11-22 10:09:26 -0300

    validando expiration time e redirecionando para url original

--- src/main/java/com/mayran/lambda/UrlData.java
+++ src/main/java/com/mayran/lambda/UrlData.java
@@ -2,10 +2,12 @@ package com.mayran.lambda;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UrlData {
     String originalUrl;
